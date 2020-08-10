6b4298cf70945dc8d5ab01c5e851ac73d9d845f0
InnovaLP <59399079+InnovaLP@users.noreply.github.com>
2020-08-10 19:52:43 -0300

    Updates

---
+++ precache-manifest.45c98036f6ea67a1b8aff5aca7e3ea3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd786c7f4dd3218aaf9886dd3facf0a",
+    "url": "/asdfgthyjkl/index.html"
+  },
+  {
+    "revision": "291e1c85ed3b52d1cab9",
+    "url": "/asdfgthyjkl/static/js/2.59d3a838.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/asdfgthyjkl/static/js/2.59d3a838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77aa6d529cbe6139ee90",
+    "url": "/asdfgthyjkl/static/js/main.43487ae0.chunk.js"
+  },
+  {
+    "revision": "9ca98210d0d542d6fafb",
+    "url": "/asdfgthyjkl/static/js/runtime-main.025ddda6.js"
+  }
+]);
